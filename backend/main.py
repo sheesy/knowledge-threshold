6fda15c85c1da297fff88babe18f88e59da0920a
Sheesy <88718851+sheesy@users.noreply.github.com>
2026-03-12 12:30:12 +0000

    fix: allow all cors origins

--- backend/main.py
+++ backend/main.py
@@ -18,11 +18,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://knowledge-threshold.vercel.app",
-        "https://sheesy.github.io",
-        "http://localhost:3000",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
